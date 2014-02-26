576f40f2fed7da475aab457f5c9b9148d4f13109
mgantola <mg.antola@gmail.com>
2014-02-25 22:45:39 -0200

    Update Conector.java

--- com.data/Conector.java
+++ com.data/Conector.java
@@ -50,5 +50,54 @@ public void desconectar() {
 	}
 }
 
+/* agregar un cliente */
+public int addCliente(String tpo_DNI, int nro_DNI,
+		String apellido, String nombre, String direccion, String telefono,
+		String mail, String usuario, String contraseña) throws Exception {
+
+	int claveGenerada = 0;
+	try {
+		PreparedStatement ps = conexion.prepareStatement(
+						"INSERT INTO clientes "
+								+ "(tpo_DNI,nro_DNI,apellido,nombre,direccion,telefono,mail,usuario,contrasenia) "
+								+ "VALUES (?,?,?,?,?,?,?,?,?)",
+						PreparedStatement.RETURN_GENERATED_KEYS);
+
+		ps.setString(1, tpo_DNI);
+		ps.setInt(2, nro_DNI);
+		ps.setString(3, apellido);
+		ps.setString(4, nombre);
+		ps.setString(5, direccion);
+		ps.setString(6, telefono);
+		if (mail == "") {ps.setString(7, null);} else {ps.setString(7, mail);}
+		ps.setString(8,usuario);
+		ps.setString(9,contraseña);
+		ps.executeUpdate();
+		/* Se obtiene la clave generada */
+		ResultSet rs = ps.getGeneratedKeys();
+		while (rs.next()) {claveGenerada = rs.getInt(1);}} 
+	catch (SQLException sqlEx) {
+		// TODO: handle exception 
+		if (esErrorDeClaveDuplicada(sqlEx.getErrorCode())) {
+			throw new Exception("ERROR al dar de alta al cliente "  
+					 + nombre + " " + apellido + " ,ya existe.");
+		} else {
+			throw new Exception("ERROR SQL al dar de alta al cliente " + apellido + " " + nombre
+					+ ": " + "(" + sqlEx.getErrorCode() + ")"
+					+ sqlEx.getMessage());
+		}
+	} catch (Exception e) {
+		throw new Exception("ERROR DESCONOCIDO al dar de alta a "
+				+ apellido + " " + nombre + "..." + e.getMessage());
+	}
+
+	return claveGenerada;
+}
+
+
+private boolean esErrorDeClaveDuplicada(int errorode) {
+	return errorode == 1062;
+}
+
 
 }
